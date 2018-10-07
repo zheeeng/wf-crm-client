fe77701147ec2c78de75f5d8b088b490a9efc6fd
Zheeeng <hi@zheeeng.me>
2018-10-07 09:25:03 +0800

    Update my-customers-all page main content

---
+++ src/pages/my-customers/all/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles'
+import { WithContext, ExtractContext } from '~src/utils/reactStore'
+import store from '~src/services/contacts'
+import PeopleList from '~src/components/PeopleList'
+
+const styles = (theme: Theme) => createStyles({
+})
+
+export interface Props extends
+  WithStyles<typeof styles>,
+  WithContext<
+    ExtractContext<typeof store>,
+    'contactContext'
+  > {
+  }
+
+export interface State {
+}
+
+class AllMyCustomersIndex extends React.Component<Props, State> {
+  private get contactContext () { return this.props.contactContext! }
+
+  render () {
+    const contacts = this.contactContext.contacts
+
+    return (
+      <PeopleList contacts={contacts} />
+    )
+  }
+}
+
+export default store.connect(
+  withStyles(styles)(AllMyCustomersIndex),
+  'contactContext',
+)
