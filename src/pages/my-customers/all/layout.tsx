3e8fef44a02a9a0b4ebe98bd4fb8d675c42ddabc
Zheeeng <hi@zheeeng.me>
2018-10-07 09:24:19 +0800

    Update my-customers-all page layout: pass child node

--- src/pages/my-customers/all/layout.tsx
+++ src/pages/my-customers/all/layout.tsx
@@ -1,19 +1,7 @@
 import * as React from 'react'
 
 export default class AllLayout extends React.Component {
-  handleClick = () => {
-    console.log('.', (this.props as any).commands.list('.'))
-    console.log('..', (this.props as any).commands.list('..'))
-    console.log('/', (this.props as any).commands.list('/'))
-    console.log('-', (this.props as any).commands.list('-'))
-  }
-
   render () {
-    return (
-      <div onClick={this.handleClick}>
-        AllLayout
-        {this.props.children}
-      </div>
-    )
+    return this.props.children
   }
 }
