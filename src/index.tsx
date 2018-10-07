7fc4e4dc876f02dd0a0fe9ce7e53ae54b9ed0c9e
Zheeeng <hi@zheeeng.me>
2018-10-07 09:22:28 +0800

    Use context and theme Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Roundation from 'roundation'
 import registerServiceWorker from '~src/registerServiceWorker'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#5d8df8',
+    },
+  },
+})
+
 ReactDOM.render(
-  <CssBaseline>
+  <CssBaseline><MuiThemeProvider theme={theme}>
     <Roundation />
-  </CssBaseline>,
-  document.getElementById('root') as HTMLElement,
+  </MuiThemeProvider></CssBaseline>,
+  document.querySelector('#content') as HTMLElement,
 )
 registerServiceWorker()
