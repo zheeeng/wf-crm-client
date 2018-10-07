b15eb6c27c160a7ab50e7fb6de3f3229a8696dc9
Zheeeng <hi@zheeeng.me>
2018-10-07 09:26:24 +0800

    Add Contact service

---
+++ src/services/contacts.tsx
@@ -0,0 +1,14 @@
+import { Contact } from '~src/types/Contact'
+import createStore from '~src/utils/reactStore'
+
+const store = createStore(setState => ({
+  contacts: [] as Contact[],
+  async fetchContacts () {
+    const contactsRes = await fetch('http://localhost:3080/contacts')
+    setState({
+      contacts: await contactsRes.json() as Contact[],
+    })
+  },
+}))
+
+export default store
