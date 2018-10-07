17e2ef96a27f84c2c4c6dee8f77b52e5824fa5ce
Zheeeng <hi@zheeeng.me>
2018-10-07 09:26:09 +0800

    Add my-customers-starred index page content

---
+++ src/pages/my-customers/starred/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles'
+import { WithContext, ExtractContext } from '~src/utils/reactStore'
+import store from '~src/services/contacts'
+import PeopleList from '~src/components/PeopleList'
+
+const styles = (theme: Theme) => createStyles({
+})
+
+export interface Props extends
+  WithStyles<typeof styles>,
+  WithContext<
+    ExtractContext<typeof store>,
+    'contactContext'
+  > {
+  }
+
+export interface State {
+}
+
+class StarredMyCustomersIndex extends React.Component<Props, State> {
+  private get contactContext () { return this.props.contactContext! }
+
+  componentDidMount () {
+    this.contactContext.fetchContacts()
+  }
+
+  render () {
+    const contacts = this.contactContext.contacts.filter(contact => contact.info.starred)
+
+    return (
+      <PeopleList contacts={contacts} />
+    )
+  }
+}
+
+export default store.connect(
+  withStyles(styles)(StarredMyCustomersIndex),
+  'contactContext',
+)
