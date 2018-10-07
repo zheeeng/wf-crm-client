238a68d92de2a75c0b89b1af6f4ba8fef1815b17
Zheeeng <hi@zheeeng.me>
2018-10-07 09:19:51 +0800

    Refactor main layout

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,18 +1,5 @@
 import * as React from 'react'
 import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles'
-import Drawer from '@material-ui/core/Drawer'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import Divider from '@material-ui/core/Divider'
-import BeachAccessIcon from '@material-ui/icons/BeachAccess'
-import StarIcon from '@material-ui/icons/Star'
-
-const drawerWidth = 240
-
-const mailFolderListItems = ['Contacts']
-const otherMailFolderListItems = ['All', 'Starred', 'Groups']
 
 const styles = (theme: Theme) => createStyles({
   root: {
@@ -23,14 +10,9 @@ const styles = (theme: Theme) => createStyles({
     position: 'relative',
     display: 'flex',
   },
-  drawerPaper: {
-    position: 'fixed',
-    width: drawerWidth,
-    backgroundColor: theme.palette.background.paper,
-  },
   main: {
     flexGrow: 1,
-    marginLeft: drawerWidth,
+    marginLeft: theme.spacing.unit * 30,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing.unit * 3,
     minWidth: 0,
@@ -40,7 +22,7 @@ const styles = (theme: Theme) => createStyles({
     flexDirection: 'column',
   },
   content: {
-    padding: theme.spacing.unit,
+    padding: theme.spacing.unit * 4,
     backgroundColor: theme.palette.background.paper,
     boxShadow: '0 0 5px 1px lightgrey',
     flexGrow: 1,
@@ -50,8 +32,6 @@ const styles = (theme: Theme) => createStyles({
 
 export interface Props extends WithStyles<typeof styles> {
   header?: JSX.Element | JSX.Element[]
-  footer?: JSX.Element | JSX.Element[]
-  aside?: JSX.Element | JSX.Element[]
 }
 
 class App extends React.Component<Props> {
@@ -62,35 +42,6 @@ class App extends React.Component<Props> {
     return (
       <div className={classes.root}>
         {this.props.header}
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.toolbar} />
-          <List>{mailFolderListItems.map(item => (
-            <ListItem>
-              <ListItemIcon>
-                <BeachAccessIcon />
-              </ListItemIcon>
-              <ListItemText inset>
-                {item}
-              </ListItemText>
-            </ListItem>
-          ))}</List>
-          <Divider />
-          <List>{otherMailFolderListItems.map(item => (
-            <ListItem>
-              <ListItemIcon>
-                <StarIcon />
-              </ListItemIcon>
-              <ListItemText inset>
-                {item}
-              </ListItemText>
-            </ListItem>
-          ))}</List>
-        </Drawer>
         <main className={classes.main}>
           <div className={classes.toolbar} />
           <div className={classes.content}>
