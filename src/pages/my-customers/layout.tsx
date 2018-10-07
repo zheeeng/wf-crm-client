e5d7f4228b7f5dd742d8cfe6695d386473127e8e
Zheeeng <hi@zheeeng.me>
2018-10-07 09:21:29 +0800

    Update my-customers layout

--- src/pages/my-customers/layout.tsx
+++ src/pages/my-customers/layout.tsx
@@ -1,13 +1,40 @@
 import * as React from 'react'
+import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles'
+import { WithContext, ExtractContext } from '~src/utils/reactStore'
+import store from '~src/services/contacts'
 
-export default class MyCustomersLayout extends React.Component {
+// const { Provider } = store
+
+const styles = (theme: Theme) => createStyles({
+})
+
+export interface Props extends
+  WithStyles<typeof styles>,
+  WithContext<
+  ExtractContext<typeof store>,
+    'contactContext'
+  > {
+    aside?: JSX.Element | JSX.Element[]
+  }
+
+class MyCustomersLayout extends React.Component<Props> {
+  componentDidMount () {
+    this.props.contactContext!.fetchContacts()
+  }
 
   render () {
     return (
-      <div>
-        MyCustomersLayout
+      <>
+        {this.props.aside}
         {this.props.children}
-      </div>
+      </>
     )
   }
 }
+
+export default store.inject(
+  store.connect(
+    withStyles(styles)(MyCustomersLayout),
+    'contactContext',
+    ),
+  )
