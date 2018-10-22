91dab295b2e2c99b12169d32464ffa064e47bb21
Zheeeng <hi@zheeeng.me>
2018-10-22 23:30:02 +0800

    Add groups page

---
+++ src/pages/my-customers#3/groups#3/:group/@profile/layout.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export default class ProfileLayout extends React.Component {
+  render () {
+    return (
+      <div>
+        GroupsContactProfileLayout
+        {this.props.children}
+      </div>
+    )
+  }
+}
---
+++ src/pages/my-customers#3/groups#3/:group/layout.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export default class GroupsContactLayout extends React.Component {
+  render () {
+    return (
+      <div>
+        GroupsContactLayout
+        {this.props.children}
+      </div>
+    )
+  }
+}
