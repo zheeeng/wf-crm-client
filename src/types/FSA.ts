fb0cdfea753185b46d9448ef485d7fdbfb7316e9
Zheeeng <hi@zheeeng.me>
2018-11-04 03:29:56 +0800

    Add FSA type

---
+++ src/types/FSA.ts
@@ -0,0 +1,10 @@
+export type FacebookStandardAction<T = string, P = never, M extends { error: Error } = never> =
+  [P] extends [never]
+    ? [M] extends [never]
+      ? { type: T }
+      : { type: T, meta: M }
+    : [M] extends [never]
+      ? { type: T, payload: P }
+      : { type: T, payload: P, meta: M }
+
+export type FSA<T = string, P = never, M extends { error: Error } = never> = FacebookStandardAction<T, P, M>
