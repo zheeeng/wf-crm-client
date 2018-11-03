008f3a8a055f8c7f6cdf8244998becaf41b6412f
Zheeeng <hi@zheeeng.me>
2018-11-03 15:37:09 +0800

    Update configuration

--- src/react-hooks.d.ts
+++ src/react-hooks.d.ts
@@ -1,30 +1,30 @@
 import * as React from 'react'
 
 declare module 'react' {
-  function useState<T>(initialState: T | (() => T)): [T, (newState: T) => void]
+  function useState<T> (initialState: T | (() => T)): [T, (newState: T) => void]
 
-  function useEffect(
-    create: () => void|Promise<void> | (() => void)|Promise<() => void>,
+  function useEffect (
+    create: () => void|Promise<void> | (() => void) | Promise<() => void>,
     inputs?: ReadonlyArray<unknown>
   ): void
 
-  function useContext<T>(foo: React.Context<T>): T
+  function useContext<T> (foo: React.Context<T>): T
 
-  function useReducer<S, A>(
+  function useReducer<S, A> (
     reducer: (state: S, action: A) => S,
     initialState: S
   ): [S, (action: A) => void]
 
-  function useCallback<F extends (...args: never[]) => unknown>(
+  function useCallback<F extends (...args: never[]) => unknown> (
     callback: F,
     inputs?: ReadonlyArray<unknown>
   ): F
 
-  function useMemo<T>(create: () => T, inputs?: ReadonlyArray<unknown>): T
+  function useMemo<T> (create: () => T, inputs?: ReadonlyArray<unknown>): T
 
-  function useRef<T extends unknown>(initialValue?: T): React.RefObject<T>
+  function useRef<T extends unknown> (initialValue: T): React.RefObject<T>
 
-  function useImperativeMethods<T>(
+  function useImperativeMethods<T> (
     ref: React.Ref<T>,
     createInstance: () => T,
     inputs?: ReadonlyArray<unknown>
