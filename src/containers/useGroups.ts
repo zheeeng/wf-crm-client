9844d8e5dac83e3d63f7da519062a7e5cb9e7c51
Zheeeng <hi@zheeeng.me>
2018-12-30 08:11:36 +0800

    Add useGroups hook

---
+++ src/containers/useGroups.ts
@@ -0,0 +1,24 @@
+import { useCallback, useMemo } from 'react'
+import { useGet } from '~src/hooks/useRequest'
+import { GroupAPI, groupInputAdapter } from '~src/types/Contact'
+
+const useGroups = () => {
+  const { data: groupsData, request: getGroupsData } = useGet<GroupAPI[]>()
+
+  const fetchGroups = useCallback(
+    () => { getGroupsData('/api/group')() },
+    [],
+  )
+
+  const groups = useMemo(
+    () => (groupsData || []).map(groupInputAdapter),
+    [groupsData],
+  )
+
+  return {
+    groups,
+    fetchGroups,
+  }
+}
+
+export default useGroups
