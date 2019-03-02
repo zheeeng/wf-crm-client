143862b688445533d13db30abfa49c8596428de1
Zheeeng <hi@zheeeng.me>
2019-03-03 07:51:05 +0800

    Add setDomain util

---
+++ src/utils/setDomain.ts
@@ -0,0 +1,11 @@
+export default function setDomain () {
+  const originalDomainChunks = document.domain.split('.')
+  try {
+    while (originalDomainChunks.length > 2 && true) {
+      originalDomainChunks.shift()
+      document.domain = originalDomainChunks.join('.')
+    }
+  } catch {
+    return
+  }
+}
