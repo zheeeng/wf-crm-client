92064f1126a617e8ebb65674730c9d785f89fcb4
Zheeeng <hi@zheeeng.me>
2019-03-16 20:04:44 +0800

    Update build process

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,8 @@
+const { override, addWebpackAlias } = require('customize-cra')
 const path = require('path')
 
-module.exports = function override (config, env) {
-  const extendAlias = {
-    '~src': path.resolve(__dirname, 'src')
-  }
-
-  if (config.resolve) {
-    if (config.resolve.alias) {
-      Object.assign(config.resolve.alias, extendAlias)
-    } else {
-      config.resolve.alias = extendAlias
-    }
-  } else {
-    config.resolve = {
-      alias: extendAlias
-    }
-  }
-
-  return config
-}
+module.exports = override(
+  addWebpackAlias({
+    '~src': path.resolve(__dirname, 'src'),
+  })
+)
