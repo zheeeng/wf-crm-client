d5572ad07bc9409547501957cce78aae6d4774ef
Zheeeng <hi@zheeeng.me>
2019-03-29 03:19:08 +0800

    Update routing

--- src/pages/-footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer: React.FC = React.memo(() => (
-  <div>
-    Footer
-  </div>
-))
-
-export default Footer
---
+++ src/pages/crm/layout.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import { Theme } from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import { ComponentProps } from '@roundation/roundation/lib/types'
+
+import * as vars from '~src/theme/vars'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flex: 1,
+    height: '100%',
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  main: {
+    flex: 1,
+    marginLeft: vars.SiderBarWidth,
+    [theme.breakpoints.down('md')]: {
+      marginLeft: 0,
+    },
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    minWidth: 0,
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+}))
+
+
+import ContactsCountContainer from '~src/containers/ContactsCount'
+import GroupsContainer from '~src/containers/Groups'
+export interface Props extends
+  ComponentProps<'aside' | 'header'> {
+}
+
+const CRMLayout: React.FC<Props> = ({ slots, children }) => {
+  const classes = useStyles({})
+
+  return (
+    <ContactsCountContainer.Provider>
+      <GroupsContainer.Provider>
+      <div className={classes.root}>
+        {slots.header}
+        <div className={classes.main}>
+          <Toolbar variant="dense" />
+          {slots.aside}
+          {children}
+        </div>
+      </div>
+      </GroupsContainer.Provider>
+    </ContactsCountContainer.Provider>
+  )
+}
+
+export default CRMLayout
