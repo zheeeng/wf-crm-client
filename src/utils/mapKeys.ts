2e088da0602f3b12d4694a0b5ea33973a3cccafd
Zheeeng <hi@zheeeng.me>
2019-04-06 23:55:52 +0800

    Handle null or undefined situation

--- src/utils/mapKeys.ts
+++ src/utils/mapKeys.ts
@@ -1,4 +1,6 @@
 export default function mapKeys <T>(mapper: (i : string) => string, obj: object) {
+  if (obj === undefined || obj === null) return obj
+
   return Object.entries(obj).reduce(
     (acc, [k, v]) => {
       (acc as any)[mapper(k)] = v
