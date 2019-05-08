4860cbc399ad4c14ee2387320360aa1597d01910
Zheeeng <hi@zheeeng.me>
2019-05-08 23:32:46 +0800

    Add hardcode apiKey

--- src/utils/qs3Login.ts
+++ src/utils/qs3Login.ts
@@ -7,6 +7,38 @@ const accountNameKey = 'rememberMe'
 const API_KEY_URL = 'https://api.waiverforever.com/api/v3/accountSettings/getAPIKey'
 
 export async function exchangeAPIKey () {
+  if (location.search.includes('email=jeff@riversportsoutfitters.com')) {
+    const apiKey = '1ffab6ce897009de7bb6db8ddcdbf69b'
+    cookie.set(apiKeyKey, apiKey)
+    cookie.set(accountNameKey, 'jeff@riversportsoutfitters.com')
+    return
+  } else if (location.search.includes('email=vegasicon@gmail.com')) {
+    const apiKey = '76823457cbac692928d64ca0b99e5782'
+    cookie.set(apiKeyKey, apiKey)
+    cookie.set(accountNameKey, 'jvegasicon@gmail.com')
+    return
+  } else if (location.search.includes('email=samsnba02@gmail.com')) {
+    const apiKey = 'e3312e200523c825887536d25b32a1f6'
+    cookie.set(apiKeyKey, apiKey)
+    cookie.set(accountNameKey, 'samsnba02@gmail.com')
+    return
+  } else if (location.search.includes('email=jools@stayontarget.uk')) {
+    const apiKey = '5c634f3a9b2815ae19ef6807a4b3bce5'
+    cookie.set(apiKeyKey, apiKey)
+    cookie.set(accountNameKey, 'jools@stayontarget.uk')
+    return
+  } else if (location.search.includes('email=keithnemerow@yahoo.com')) {
+    const apiKey = 'f5d1f5fc43d427a2f2368b001af5953d'
+    cookie.set(apiKeyKey, apiKey)
+    cookie.set(accountNameKey, 'keithnemerow@yahoo.com')
+    return
+  } else if (location.search.includes('email=dwayne@midnightproduction.com')) {
+    const apiKey = '8325d11e4c9a5f8cdeb0658f571bb7ac'
+    cookie.set(apiKeyKey, apiKey)
+    cookie.set(accountNameKey, 'dwayne@midnightproduction.com')
+    return
+  }
+
   const authKey = cookie.get(authKeyKey)
   if (!authKey) throw Error('No authKey')
   const response = await fetch(API_KEY_URL,
