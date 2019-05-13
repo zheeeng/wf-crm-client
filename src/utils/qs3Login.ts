363d33dbfdbe44563fbca1a99354172b802bf813
Zheeeng <hi@zheeeng.me>
2019-05-13 22:22:48 +0800

    Support dev login info

--- src/utils/qs3Login.ts
+++ src/utils/qs3Login.ts
@@ -4,6 +4,12 @@ const apiKeyKey = 'apiKey'
 const authKeyKey = 'authKey'
 const accountNameKey = 'rememberMe'
 
+const isDev = process.env.NODE_ENV === 'development'
+const devLoginInfo = {
+  api_key: '',
+  email: 'a',
+  password: '0cc175b9c0f1b6a831c399e269772661',
+}
 const API_KEY_URL = 'https://api.waiverforever.com/api/v3/accountSettings/getAPIKey'
 
 export async function exchangeAPIKey () {
@@ -69,6 +75,8 @@ export function clean () {
 }
 
 export function getLoginParams () {
+  if (isDev) return devLoginInfo
+
   return {
     email: cookie.get(accountNameKey),
     api_key: cookie.get(apiKeyKey),
