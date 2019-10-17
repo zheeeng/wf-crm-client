8f4b32d4680b741515be94c6cde690a47f4ff58c
Zheeeng <hi@zheeeng.me>
2019-10-18 06:08:56 +0800

    Add config for localCache hook

---
+++ src/hooks/localCacheConfig.ts
@@ -0,0 +1,4 @@
+import TimeUnit from '~src/utils/TimeUnit'
+
+export const localStorageKeyForDeletingContacts = 'DeleteContactList'
+export const localStorageExpirationForDeletingContacts = TimeUnit.Minute * 30
