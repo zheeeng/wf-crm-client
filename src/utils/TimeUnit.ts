1bec69e59cbadeeffa8f1250ea879d8987d13cdb
Zheeeng <hi@zheeeng.me>
2019-10-18 06:08:01 +0800

    Extract TimeUnit

---
+++ src/utils/TimeUnit.ts
@@ -0,0 +1,10 @@
+enum TimeUnit {
+    Ms = 1,
+    Second = TimeUnit.Second * 1000,
+    Minute = TimeUnit.Second * 60,
+    Hour = TimeUnit.Minute * 60,
+    Day = TimeUnit.Hour * 24,
+    Week = TimeUnit.Day * 7,
+}
+
+export default TimeUnit
