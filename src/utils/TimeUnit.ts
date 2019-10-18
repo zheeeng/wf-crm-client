29c0cd8a7ba30a5e8978027f8c4a093ddaf77b11
Zheeeng <hi@zheeeng.me>
2019-10-18 08:26:31 +0800

    fix TimeUnit

--- src/utils/TimeUnit.ts
+++ src/utils/TimeUnit.ts
@@ -1,6 +1,6 @@
 enum TimeUnit {
     Ms = 1,
-    Second = TimeUnit.Second * 1000,
+    Second = TimeUnit.Ms * 1000,
     Minute = TimeUnit.Second * 60,
     Hour = TimeUnit.Minute * 60,
     Day = TimeUnit.Hour * 24,
