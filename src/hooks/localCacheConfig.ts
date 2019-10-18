11f44f1ca1d08a4da00be11b0488fb2894a13284
Zheeeng <hi@zheeeng.me>
2019-10-18 08:27:21 +0800

    Move business config

--- src/hooks/localCacheConfig.ts
+++
@@ -1,4 +0,0 @@
-import TimeUnit from '~src/utils/TimeUnit'
-
-export const localStorageKeyForDeletingContacts = 'DeleteContactList'
-export const localStorageExpirationForDeletingContacts = TimeUnit.Minute * 30
---
+++ src/meta/localCacheConfig.ts
@@ -0,0 +1,10 @@
+import TimeUnit from '~src/utils/TimeUnit'
+
+export const localStorageKeyForDeletingContacts = 'DeleteContactList'
+export const localStorageExpirationForDeletingContacts = TimeUnit.Minute * 30
+export function delContactRecordKey (contactId: string) {
+    return `del:${contactId}`
+}
+export function delContactRecordPattern () {
+    return /^del:/
+}
